d577b365291424f268c48440129e96252d8cf01c
SKarolinav <Skkarolinav@gmail.com>
2019-06-02 21:39:00 +0200

    Updates

---
+++ precache-manifest.03980ce4b644bf816de83971e3d61c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf5fe007d6c08e23be6457b202f7e36",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "1d6e5627110a91e5aada",
+    "url": "/weather-app/static/css/main.7b6cb855.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/weather-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1d6e5627110a91e5aada",
+    "url": "/weather-app/static/js/main.f1078eaf.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "c6be65dc35af0d582893fb4ed62c2ea4",
+    "url": "/weather-app/static/media/Wallpaper.c6be65dc.jpg"
+  }
+]);
