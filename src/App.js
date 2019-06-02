40936d838b583a57a0f43db7ff12e6547ea0d9df
SKarolinav <Skkarolinav@gmail.com>
2019-06-02 21:17:57 +0200

    weather application v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Search from './components/search/Search'
+import Result from './components/result/Result'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const APIKey = 'c15e19c5524c0e99c9ad66fd1632e8bf'
+
+class App extends Component{
+  state={
+    value:'',
+    date:'',
+    city:'',
+    sunrise:'',
+    sunset:'',
+    temp:'',
+    pressure:'',
+    wind:'',
+    err: false,
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleCitySubmit = (e) => {
+    e.preventDefault()
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+
+    fetch(API)
+    .then(response => {
+      if(response.ok){
+        return response
+      }
+      throw Error('Error trying to get the data')
+    })
+    .then(response => response.json())
+    .then (data => {
+      const time = new Date().toLocaleString()
+      this.setState(prevState =>({
+        err: false,
+        date:time,
+        city:prevState.value,
+        sunrise: data.sys.sunrise,
+        sunset:data.sys.sunset,
+        temp:data.main.temp,
+        pressure:data.main.pressure,
+        wind:data.wind.speed,
+      }))
+    })
+    .catch(err => {
+      console.log(err)
+      this.setState(prevState => ({
+        err: true,
+        city: prevState.value
+      }))
+    })
+  }
+
+  render(){
+    return (
+    
+        <div className='container'>
+          <div className='mycontainer'>
+              <Search 
+                value={this.state.value} 
+                handleInputChange={this.handleInputChange}
+                handleCitySubmit={this.handleCitySubmit}
+              />
+          </div>
+
+            <Result 
+              weather={this.state}
+            />
+        </div>
+    
+    )
+  }
 }
 
 export default App;
---
+++ src/components/result/Result.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Result = (props) => {
+
+    const{date, city, sunrise, sunset, temp, pressure, wind, err} = props.weather
+
+    let content= null;
+
+    if(!err && city){
+        const sunriseTime = new Date(sunrise*1000).toLocaleTimeString()
+        const sunsetTime = new Date(sunset*1000).toLocaleTimeString()
+        
+        content = (
+            <div className='row'>
+                <div className='col col-12 col-md-6 mycolumn1'>
+                    <p className='city'>City: <strong>{city}</strong></p>
+                    <p>Date and time: {date}</p>
+                </div>
+                <div className='row line'></div>
+                <div className='col col-12 col-md-6 mycolumn2'>
+                    <p>Temperature: <strong>{temp} &#176;C</strong></p>
+                    <p>Sunrise: <strong>{sunriseTime}</strong></p>
+                    <p>Sunset: <strong>{sunsetTime}</strong></p>
+                    <p>Pressure: <strong>{pressure} hPa</strong></p>
+                    <p>Wind velocity: <strong>{wind} m/s</strong></p>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            {err ? `There is no such a city in the database: ${city}` : content}
+        </>
+      )
+}
+ 
+export default Result;
